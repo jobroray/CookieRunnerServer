edd3f19003344db47f43a5f4b6fc15e4d8efc0ac
Josiah Ray <josiahjray@gmail.com>
2026-06-10 22:52:06 -0500

    api testing

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Cookie Runner Backend is live and running!');
+});
+
 const token = process.env.SQUARE_ACCESS_TOKEN;
 console.log("=== SQUARE TOKEN DIAGNOSTICS ===");
 console.log("1. Token actually exists inside Render?", !!token);
